8442fc0c87965735d383c48a62adeb77e4ef0f41
James Putnam <jonathan.putnam86@gmail.com>
2016-06-03 18:17:47 -0600

    My first commit

---
+++ js/app.js
@@ -0,0 +1,34 @@
+angular.module('nzApp', ['ui.router'])
+  .config(function($stateProvider, $urlRouterProvider){
+      $urlRouterProvider.otherwise('/');
+      $stateProvider
+        .state('welcome', {
+            url:'/',
+            templateUrl: "./home/home.html"
+        })
+        .state('about', {
+          url:'/about',
+          templateUrl: './about/about'
+        })
+        .state('northIsland',{
+          url:'/northIsland',
+          templateUrl: './northIsland/northIsland.html'
+        })
+        .state('southIsland',{
+          url:'/southIsland',
+          templateUrl: './southIsland/southIsland.html'
+        })
+        .state('travelPlans',{
+          url:'/travelPlans',
+          templateUrl: './travelPLans/travelPlans.html'
+        })
+        .state('weather', {
+          url: '/weather',
+          templateUrl: './weather/weather.html'
+        });
+
+
+
+
+
+});
---
+++ js/weatherService.js
@@ -0,0 +1,6 @@
+angular.module('nzApp').service('weatherService', function(){
+
+
+
+
+});
